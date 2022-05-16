aca8ef57186ade8d16c33b648f07c82a10ba168b
Lincoln S. Rocha <lincoln@dc.ufc.br>
2022-05-16 16:42:14 -0300

    fixing bugs in faulty semantic inputs and in symbol table

--- tests/testdata/faulty/semantic/SemanticFaultyUndeclared.java
+++ tests/testdata/faulty/semantic/SemanticFaultyUndeclared.java
@@ -1,20 +1,19 @@
 class A {
     public static void main(String[] args){
-        C obj1;
-        B obj2;
-
-        obj1 = new C();
-        obj2 = new B();
-
-		System.out.println(obj2.twoMethod());
+        {
+            C obj1;
+            B obj2;
+            obj1 = new C();
+            obj2 = new B();
+		    System.out.println(obj2.twoMethod());
+        }
     }
 }
 
 class B extends D {
 
     public int oneMethod(){
-        obj = 0;
-               
+        obj = 0;      
 		return 0 ;
     }
 
