490acdc1e31074671421043a9cd535281cef58c0
Lincoln S. Rocha <lincoln@dc.ufc.br>
2022-05-16 17:56:15 -0300

    fixing bugs in faulty semantic inputs

--- tests/testdata/faulty/semantic/SemanticFaultyUndeclared.java
+++ tests/testdata/faulty/semantic/SemanticFaultyUndeclared.java
@@ -1,20 +1,23 @@
 class A {
     public static void main(String[] args){
-        {
-            C obj1;
-            B obj2;
-            obj1 = new C();
-            obj2 = new B();
-		    System.out.println(obj2.twoMethod());
-        }
+		    System.out.println(new B().oneMethod());
     }
 }
 
 class B extends D {
 
     public int oneMethod(){
-        obj = 0;      
-		return 0 ;
+        obj = 0;
+		    return 0;
+    }
+
+    public int otherMethod(){
+        C obj1;
+        B obj2;
+        obj1 = new C();
+        obj2 = new B();
+        obj2.twoMethod();
+		    return 0;
     }
 
 }
