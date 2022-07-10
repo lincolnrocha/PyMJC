c400558d53bb28ce915feea14ef1ba2fd05465d7
Lincoln S. Rocha <lincoln@dc.ufc.br>
2022-07-10 11:18:40 -0300

    minor fixies

--- pymjc/back/regalloc.py
+++ pymjc/back/regalloc.py
@@ -1,17 +1,29 @@
 from __future__ import annotations
 from abc import abstractmethod
-from pymjc.back import graph
-from pymjc.back import flowgraph
-from pymjc.front import temp
+from pymjc.back import assem, flowgraph, graph
+from pymjc.front import frame, temp
+
+class RegAlloc (temp.TempMap):
+    def __init__(self, frame: frame.Frame, instr_list: assem.InstrList):
+        self.instrs: assem.InstrList = instr_list
+        #TODO
+
+    def temp_map(self, temp: temp.Temp) -> str:
+        #TODO
+        return temp.to_string()
+    
 
 class Color(temp.TempMap):
     def __init__(self, ig: InterferenceGraph, initial: temp.TempMap, registers: temp.TempList):
+        #TODO
         pass
     
     def spills(self) -> temp.TempList:
+        #TODO
         return None
 
     def temp_map(self, temp: temp.Temp) -> str:
+        #TODO
         return temp.to_string()
 
 class InterferenceGraph(graph.Graph):
@@ -36,16 +48,20 @@ class Liveness (InterferenceGraph):
 
     def __init__(self, flow: flowgraph.FlowGraph):
         self.live_map = {}
+        #TODO
 
 
     def tnode(self, temp:temp.Temp) -> graph.Node:
+        #TODO
         return None
 
 
     def gtemp(self, node: graph.Node) -> temp.Temp:
+        #TODO
         return None
 
     def moves(self) -> MoveList:
+        #TODO
         return None
 
 
